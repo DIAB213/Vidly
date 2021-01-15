f915029cc8999e9941cff7fff2b1640dacff59d8
Abdelrahman Diab <abdelrahmandiab@outlook.com>
2021-01-15 13:18:49 +0200

    Entity framework (code first) added migrations

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,30 +11,39 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        List<Customer> customers = new List<Customer>
-        {
-            new Customer() {Name = "John Smith", Id = 1},
-            new Customer() {Name = "Mary Williams", Id = 2}
-        };
-        // GET: Customer
-        public ActionResult Index()
-        {
 
-            return View(customers);
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        public ViewResult Index()
+        {
+            var customers = _context.Customers.Include(c =>c.MembershipType).ToList();
+
+            return View(customers as List<Customer>);
 
         }
+
 
         [Route("Customers/Details/{id}")]
-        public ActionResult Details(int id) {
-            foreach (var customer in customers)
-            {
-                if (customer.Id == id)
-                {   
-                    return View(customer);
-                }
-                
-            }
-            return HttpNotFound();
+        public ActionResult Details(int id)
+        {
+
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+
+            return View(customer);
+            
         }
+
     }
 }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,24 +10,38 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = new List<Movie> { 
-
-                new Movie() {Name = "Shrek",Id = 1},
-                new Movie() {Name = "Wall-E",Id = 2}
-            };
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
 
         }
 
-        
+        public ActionResult Details(int id)
+        {
 
-        
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
-        
 
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
     }
 }
